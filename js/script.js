9ca68a0a81ea80d5f1be3840b5edd084856cae5c
DenisNec52 <148898216+DenisNec52@users.noreply.github.com>
2023-12-04 20:54:10 +0100

    modificato script js per modifica bottone welcome

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 import data from '/assets/js/data.js';
-
+let easy = getElementById(alignDifficulty).easy;
+let hard = getElementById(alignDifficulty).Hard;
+let checkbox = getElementById(labelCheckbox).value;
+let button = getElementById(proceedButton).value;
+function difficulty_selector(easy,hard) {
+    if (easy==checkbox){
+        addEventListener
+    }
+    function addEventListener<checkbox>(type: click) => console.log(ciao),
+}
 console.log(data);
