ace23d20b9f91030325adda6c5b368ea9a3c876c
Jac-Luce <luce.jac92@gmail.com>
2023-12-06 13:09:46 +0100

    Modiche css global,welcome,questions e result. Modifiche html e script result

--- pages/Result/script.js
+++ pages/Result/script.js
@@ -1,2 +1,4 @@
 const risultatoQuiz = localStorage.getItem('punteggioFinale');
-console.log(risultatoQuiz * 10 + "%");
+//console.log(risultatoQuiz * 10 + "%");
+let risultatoVisibile = risultatoQuiz * 10 + "%";
+document.getElementById("result").innerHTML = risultatoVisibile;
